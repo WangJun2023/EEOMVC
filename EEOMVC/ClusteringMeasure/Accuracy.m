3d33fad948352a485b91685680e4a0ae629fa902
Wang Jun <34850979+WangJun2023@users.noreply.github.com>
2023-11-01 10:41:52 +0800

    Delete EEOMVC directory

--- EEOMVC/ClusteringMeasure/Accuracy.m
+++
@@ -1,4 +0,0 @@
-function ACC = Accuracy(C,gt)
- C = bestMap(gt,C);
- ACC = length(find(gt == C))/length(gt);
-end
--- EEOMVC/update_Hstar.m
+++
@@ -1,5 +0,0 @@
-function  Hstar = update_Hstar(Z, lambda, M, beta, Y, Q)
-    S = Z + lambda * M + 2 * beta * Y * (Y'*Y+eps*eye(size(Y,2)))^(-0.5) * Q';
-    [U,~,V] = svd(S,'econ');
-    Hstar = U * V';
-end
